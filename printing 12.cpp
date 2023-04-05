bf82f05368ec18a9b990b5f78da5b33b343ac639
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-04-05 20:59:34 +0530

    Create printing 12.cpp

---
+++ printing 12.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+  cout << 12 ;
+  
+  return 0;
+}
+
+
+
+Output
+12
+
+
+
+"cout <" (pronounced "see-out"). 
+This is what we use to output/print anything to the screen.
